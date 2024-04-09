28927e799ec82f8b93e425a8aafed0b5498e9ecc
felipemarqs <felipemarqs@outlook.com>
2024-04-09 07:37:25 -0300

    test: Upload Files test

--- api/src/modules/wells/wells.service.ts
+++ api/src/modules/wells/wells.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import * as xlsx from 'xlsx';
 import { CreateWellDto } from './dto/create-well.dto';
 import { UpdateWellDto } from './dto/update-well.dto';
 import { WellsRepository } from 'src/shared/database/repositories/well.repositories';
@@ -29,5 +30,51 @@ export class WellsService {
 
   remove(id: number) {
     return `This action removes a #${id} well`;
+  }
+
+  async uploadSheet() {
+    let workbook = xlsx.readFile('./src/modules/wells/temp/carmo.xlsx');
+
+    let worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+    let sheetWells = [];
+
+    for (let index = 2; index < 2216; index++) {
+      sheetWells.push(worksheet['E' + index].v);
+    }
+
+    const wells = await this.wellsRepo.findAll({ select: { name: true } });
+
+    //console.log(' Quantidade de Poços do sistema', wells.length);
+
+    //console.log('Quantidade de poços da tabela', sheetWells.length);
+
+    const existingWells = [];
+
+    wells.forEach((well) => {
+      const wellFound = sheetWells.find((sheetWell) => {
+        return sheetWell === well.name;
+      });
+
+      if (wellFound) {
+        existingWells.push(wellFound);
+      }
+    });
+
+    const distinctsWells = sheetWells.filter(
+      (item) => !existingWells.includes(item),
+    );
+    console.log('Poços Distintos: ', distinctsWells);
+
+    /*  const test = distinctsWells.map((well) => ({
+      contractId: 'c6b04e59-1a97-42c7-9370-3f0d48d222de',
+      name: well,
+    })); */
+
+    // console.log(test);
+    /*  await this.wellsRepo.createMany({
+      data: test,
+    }); */
+    return 'ok por enquanto';
   }
 }
