4bfd643565e5e836362d3548f2d7f7337b6a36d2
Felipe Marques <felipe.marqs@outlook.com>
2024-04-10 00:36:30 +0000

    feat: :recycle: Dashboard and Invoicing menu refactored

---
+++ fe/src/view/components/MenuCard.tsx
@@ -0,0 +1,51 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardTitle,
+} from "@/components/ui/card";
+import { LineChart, LucideIcon } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+
+interface MenuCardProps {
+  title: string;
+  Icon: React.ElementType;
+  description?: string;
+  navigateTo: string;
+}
+
+export const MenuCard = ({
+  Icon,
+  navigateTo,
+  title,
+  description,
+}: MenuCardProps) => {
+  const navigate = useNavigate();
+
+  return (
+    <Card className="w-[350px] min-h-[224px]">
+      <CardHeader className="flex flex-row gap-6  justify-start h-36 ">
+        <div className="">
+          <Icon size={30} />
+        </div>
+        <div className="flex flex-col gap-2">
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{description}</CardDescription>
+        </div>
+      </CardHeader>
+      <CardContent></CardContent>
+      <CardFooter className="flex justify-between">
+        <Button
+          className="w-full"
+          variant="secondary"
+          onClick={() => navigate(`/${navigateTo}`)}
+        >
+          Abrir
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
--- fe/src/view/pages/DashboardMenu/index.tsx
+++ fe/src/view/pages/DashboardMenu/index.tsx
@@ -1,19 +1,10 @@
-import {BarChartBig, LineChart} from "lucide-react";
-import {Header} from "../../components/Header";
-import {useDashboardMenu} from "./useDashboardMenu";
-
-import {Button} from "@/components/ui/button";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { BarChartBig, LineChart } from "lucide-react";
+import { Header } from "../../components/Header";
+import { useDashboardMenu } from "./useDashboardMenu";
+import { MenuCard } from "@/view/components/MenuCard";
 
 export const DashboardMenu = () => {
-  const {navigate, isUserAdm} = useDashboardMenu();
+  const { isUserAdm } = useDashboardMenu();
 
   return (
     <div className="w-full h-full overflow-y-scroll">
@@ -21,52 +12,22 @@ export const DashboardMenu = () => {
 
       <div className="w-full flex justify-center items-center">
         <div className=" w-full mt-12 flex flex-col justify-start items-center gap-4 lg:pl-8 lg:flex-row">
-          <Card className="w-[350px] min-h-[224px]">
-            <CardHeader className="flex flex-row gap-6  justify-start h-36 ">
-              <div className="">
-                <LineChart size={30} />
-              </div>
-              <div className="flex flex-col gap-2">
-                <CardTitle>Dashboard por Sonda</CardTitle>
-                <CardDescription>
-                  Acesse o painel específico da sonda para visualizar dados
-                  detalhados.
-                </CardDescription>
-              </div>
-            </CardHeader>
-            <CardContent></CardContent>
-            <CardFooter className="flex justify-between">
-              <Button className="w-full" onClick={() => navigate("/dashboard")}>
-                Abrir
-              </Button>
-            </CardFooter>
-          </Card>
-
           {isUserAdm && (
-            <Card className="w-[350px] min-h-[224px]">
-              <CardHeader className="flex flex-row gap-6  justify-start   h-36">
-                <div>
-                  <BarChartBig size={30} />
-                </div>
-                <div className="flex flex-col gap-2">
-                  <CardTitle>Dashboard Geral</CardTitle>
-                  <CardDescription>
-                    Explore o painel global para análises abrangentes e insights
-                    sobre o desempenho geral.
-                  </CardDescription>
-                </div>
-              </CardHeader>
-              <CardContent></CardContent>
-              <CardFooter className="flex justify-between">
-                <Button
-                  className="w-full"
-                  onClick={() => navigate("/global-dashboard")}
-                >
-                  Abrir
-                </Button>
-              </CardFooter>
-            </Card>
+            <MenuCard
+              title="Dashboard Geral"
+              Icon={BarChartBig}
+              description="Explore o painel global para análises abrangentes e insights
+            sobre o desempenho geral."
+              navigateTo="global-dashboard"
+            />
           )}
+          <MenuCard
+            title="Dashboard por Sonda"
+            Icon={LineChart}
+            description="Acesse o painel específico da sonda para visualizar dados
+                  detalhados."
+            navigateTo="dashboard"
+          />
         </div>
       </div>
     </div>
