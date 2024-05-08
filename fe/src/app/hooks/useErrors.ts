76131f564cbdd228d725264bb7d5587b6642586d
Felipe Marques <felipe.marqs@outlook.com>
2024-05-08 11:54:44 +0000

    docs: :memo: Adds docs comments to the useErrors Hook

--- fe/src/app/hooks/useErrors.ts
+++ fe/src/app/hooks/useErrors.ts
@@ -2,9 +2,27 @@ import {useState} from "react";
 
 export type ErrorArgs = {fieldName: string; message: string};
 
+/**
+ * Hook for managing form field errors.
+ * Keeps track of errors associated with form fields.
+ * Provides functions to set, remove and retrieve error messages.
+ * @returns An object containing functions to manage errors and the current error list.
+ * @example
+ * // Example usage:
+ * const { setError, errors, removeError, getErrorMessageByFieldName } = useErrors();
+ * setError({ fieldName: 'email', message: 'Invalid email format' });
+ * removeError('email');
+ * const errorMessage = getErrorMessageByFieldName('email');
+ */
+
 export const useErrors = () => {
   const [errors, setErrors] = useState<Array<ErrorArgs>>([]);
 
+  /**
+   * Sets an error message for a specific form field.
+   * @param fieldName - The name of the form field.
+   * @param message - The error message to set.
+   */
   const setError = ({fieldName, message}: ErrorArgs) => {
     const errorAlreadyExists = errors.find(
       (error) => error.fieldName === fieldName
@@ -15,12 +33,21 @@ export const useErrors = () => {
     setErrors((prevState) => [...prevState, {fieldName, message}]);
   };
 
+  /**
+   * Removes the error message associated with a specific form field.
+   * @param fieldName - The name of the form field.
+   */
   const removeError = (fieldName: string) => {
     setErrors((prevState) =>
       prevState.filter((error) => error.fieldName !== fieldName)
     );
   };
 
+  /**
+   * Retrieves the error message associated with a specific form field.
+   * @param fieldName - The name of the form field.
+   * @returns The error message for the specified field, or an empty string if no error message is found.
+   */
   const getErrorMessageByFildName = (fieldName: string) => {
     let findErrorMessage = errors.find(
       (error) => error.fieldName === fieldName
