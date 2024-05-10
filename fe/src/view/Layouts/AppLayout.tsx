79e20327d778b41db79c1a02e55afc7c1c4b712a
Felipe Marques <felipe.marqs@outlook.com>
2024-05-10 14:12:21 +0000

    feat: :sparkles: System Version created
    This creates a system version container that will show the current system's version in the bottom right of the screen

--- fe/src/view/Layouts/AppLayout.tsx
+++ fe/src/view/Layouts/AppLayout.tsx
@@ -1,5 +1,19 @@
-import { Outlet } from "react-router-dom";
-import { Navbar } from "@/view/components/Navbar";
+import {Outlet} from "react-router-dom";
+import {Navbar} from "@/view/components/Navbar";
+import {cn} from "@/lib/utils";
+import {currentVersion} from "@/app/config/CurrentVersion";
+
+export const SystemVersion = () => {
+  return (
+    <div
+      className={cn(
+        "fixed   text-sm text-gray-400 p-1 lg:p-4 z-50 rounded-lg bottom-2 right-5"
+      )}
+    >
+      <span>{currentVersion.version}</span>
+    </div>
+  );
+};
 
 export const AppLayout = () => {
   //const {activeItem,handleNavItemChange} = useMainLayout()
@@ -7,6 +21,9 @@ export const AppLayout = () => {
   return (
     <div className=" w-full h-full flex flex-col">
       <Navbar />
+
+      <SystemVersion />
+
       <Outlet />
     </div>
   );
