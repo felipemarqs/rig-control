8a970342b24f4e135d430e5ba23605f71e66feac
Felipe Marques <felipe.marqs@outlook.com>
2024-05-13 12:17:00 +0000

    refactor: :coffin: Removed a duplicated code in useContract
    This removes the ternary condition to check the  user Access Level in controller because it already exists in the context

--- fe/src/view/pages/Contract/useContract.ts
+++ fe/src/view/pages/Contract/useContract.ts
@@ -3,10 +3,8 @@ import {useAuth} from "../../../app/hooks/useAuth";
 import {useNavigate} from "react-router-dom";
 
 export const useContract = () => {
-  const {user} = useAuth();
+  const {isUserAdm} = useAuth();
   const navigate = useNavigate();
-
-  const isUserAdm = user?.accessLevel === "ADM";
 
   const {contracts, isFetchingContracts} = useContracts(isUserAdm);
 
